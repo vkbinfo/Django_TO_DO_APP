22152f59e53dffe9f422eab9595e6fd6720d7af1
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-05-06 17:13:34 +0530

    Tried Model class based form, which is awesome.

--- todo/todoForm.py
+++ todo/todoForm.py
@@ -1,11 +1,24 @@
 from django import forms
-
+from .models import Todo
 
 class TodoForm(forms.Form):
-    text_input = forms.CharField(max_length=40,
+    text = forms.CharField(max_length=40,
                                  widget=forms.TextInput(
                                      attrs={'class': 'form-control',
                                             'placeholder': "Enter your todo Item. ex: Learn something, Watch yourself",
                                             'arial-label': 'Todo',
                                             'aria-describedby': 'add-btn'})
                                  )
+
+# the awesome model(table) based form
+class NewToDoForm(forms.ModelForm):
+
+    class Meta:
+        model = Todo
+        fields = ['text']
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'form-control',
+                                            'placeholder': "Enter your todo Item. ex: Learn something, Watch yourself",
+                                            'arial-label': 'Todo',
+                                            'aria-describedby': 'add-btn'})
+        }
--- todo/views.py
+++ todo/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import Todo
-from .todoForm import TodoForm
+from .todoForm import TodoForm, NewToDoForm
 # Create your views here.
 
 
 def index(request):
     todo_list = Todo.objects.all()
-    form = TodoForm()
-    context = {'todo_list': todo_list, 'form': form}
+    # normal form
+    # form = TodoForm()
+
+    # model(table) class based form, Which is better from above TodoForm
+    modeltodoform = NewToDoForm()
+    context = {'todo_list': todo_list, 'form': modeltodoform}
     return render(request, 'todo/index.html', context)
 
 
 @require_POST
 def add_todo(request):
-    formData = TodoForm(request.POST)
-    if formData.is_valid():
-        new_todo = Todo(text=formData.cleaned_data['text_input'])
-        new_todo.save()
+    # processing in normal form class
+    # formData = TodoForm(request.POST)
+    # if formData.is_valid():
+    #     new_todo = Todo(text=formData.cleaned_data['text_input'])
+    #     new_todo.save()
+
+    # let's use our model based class form to
+    class_form_data = NewToDoForm(request.POST)
+    # if we can give instance of a model object, as second argument in above line, that we can update that instance.
+    # isn't it awesome.
+    if class_form_data.is_valid():
+        # because our class_form_data has already referencd model through form, we can just directly save that
+        class_form_data.save()
     return redirect('index')
 
 
